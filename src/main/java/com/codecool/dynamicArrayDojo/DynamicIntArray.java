0a62f821119bc88687e20aba044eec7b133b8484
Adelaiten <karoltrzaska19@gmail.com>
2019-02-11 14:36:56 +0100

    add item to an array method

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,14 +1,40 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
+
 public class DynamicIntArray {
 
+    private int array[];
+    private int index;
+
+    public DynamicIntArray() {
+        this.array = new int[10];
+        this.index = 0;
+    }
+
+
     public DynamicIntArray(int size) {
+        this.array = new int[size];
+        this.index = 0;
+    }
+
+
+    public void add(int number) {
+        if(index == array.length) {
+            resizeAnArray();
+            this.array[index++] = number;
+        }else {
+            this.array[index++] = number;
+        }
 
     }
 
-    public void add(int number) {
-
+    private void resizeAnArray() {
+        int RESIZED_ARRAY_LENGTH = this.array.length * 2;
+        int tempArray[] = this.array.clone();
+        this.array = new int[RESIZED_ARRAY_LENGTH];
+        for(int i = 0; i < tempArray.length; i++) {
+            this.array[i] = tempArray[i];
+        }
     }
 
 
