b2fcee470a3f0dc5e4d0b9fc16e18641ad9ef516
Adelaiten <karoltrzaska19@gmail.com>
2019-02-11 13:59:32 +0100

    init branch

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,4 +3,23 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
+    public DynamicIntArray(int size) {
+
+    }
+
+    public void add(int number) {
+
+    }
+
+
+    public void remove(int index) {
+
+    }
+
+
+    public void insert(int index, int number) {
+
+    }
+
+
 }
